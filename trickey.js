39217d63534cdccacfff542df3da05f1f1db2639
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-09-01 01:47:18 -0400

    trickey

---
+++ trickey.js
@@ -0,0 +1,21 @@
+function trickyFUnction() {
+    var a = [];
+    var b = a;
+    // @ts-ignore
+    a[0] = a = [0];
+    // @ts-ignore
+    b[1] = 1;
+    // @ts-ignore
+    var result = (a == b) + ({} + {});
+    var x = [0];
+    x[x[0]] = 1;
+    result += x;
+    var c = [];
+    // @ts-ignore
+    c[0] = c = [0];
+    // @ts-ignore
+    c[1] = 1;
+    result += c == c;
+    return result;
+}
+console.log(trickyFUnction());
